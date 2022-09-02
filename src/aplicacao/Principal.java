fba4060bb3945f6036e696e1297bd4ecfa23c7e6
pedroduarte <p.fmarket1996@gmail.com>
2022-09-02 15:43:10 -0300

    Inserindo os dados e retornando o id da linha afetada

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -2,12 +2,14 @@ package aplicacao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import database.DB;
-import database.DbException;
+
 
 public class Principal {
 	
@@ -31,14 +33,14 @@ public class Principal {
 					"INSERT INTO evento_comanda"
 					+ "(ID, CD_PERSON, NR_COMANDA, CD_AREA_EVT, TP_PERSON, DH_ATD, CD_USU_ATD, TP_SIT, DH_ATU, CD_USU_ATU)" 
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");//Particulariedade do Java para inserir os dados que forem necessários
+					+ "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);//Particulariedade do Java para inserir os dados que forem necessários e retornar o id da linha que foi inserida
 			
 			//Variável.setTipoDoDado(Posição, DadoInserido)
-			st.setInt(1, 2);
-			st.setLong(2, 2200);
-			st.setLong(3,1002);
+			st.setInt(1, 3);
+			st.setLong(2, 2300);
+			st.setLong(3,1003);
 			st.setLong(4, 155);
-			st.setString(5,"SÓCIO DEPENDENTE");
+			st.setString(5, "CONVIDADO");
 			st.setDate(6, new java.sql.Date(sdf.parse("2022/09/02").getTime()));
 			st.setLong(7, 6321);
 			st.setBoolean(8, false);
@@ -46,10 +48,19 @@ public class Principal {
 			st.setLong(10, 2626);
 			
 			//Variável para executar a inserção, no banco
-			int linhaAfetadas = st.executeUpdate();
+			int linhasAfetadas = st.executeUpdate();
 			
-			//Verificação se executou
-			System.out.println("Pronto, o total de linhas afetadas foi ! " + linhaAfetadas);
+			//Nesse caso faremos isso para retornar o id da linha que foi inserida
+			if(linhasAfetadas > 0) {
+				ResultSet rt = st.getGeneratedKeys();
+				while(rt.next()) {
+					int id = rt.getInt(1);
+					System.out.println("Pronto, o ID é = " + id);
+				}
+			}
+			else{
+				System.out.println("Nenhuma linha afetada !");
+			}
 			
 		}//Restante do tratamento de exceções pegando as exceções específicas
 		catch(SQLException e) {
